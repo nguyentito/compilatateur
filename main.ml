12b42aa68591ce1e485e53f37229ee7a62f35a89
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2012-11-27 21:53:34 +0100

    Gestion basique des erreurs...

--- main.ml
+++ main.ml
@@ -8,11 +8,6 @@ let option_list = ["-parse-only", Arg.Set parse_only,
                   ]
 
 
-let exn_msg = function
-  | Failure _ -> "Lexical error"
-  | Parser.Error -> "Parse error"
-  | exn -> Printexc.to_string exn
-
 let signal_failure lexpos message =
   Printf.eprintf "File \"%s\", line %d, characters %d-%d :\n%s\n"
                  lexpos.Lexing.pos_fname lexpos.Lexing.pos_lnum
@@ -23,24 +18,25 @@ let signal_failure lexpos message =
 let main_exec filename = 
   try 
     let file = open_in filename in 
-    begin 
-      let buf = Lexing.from_channel file in
+
+    let buf = Lexing.from_channel file in
+    try
       let file_parsed = Parser.parse_source_file Lexer.get_token buf in 
       if !parse_only then () 
       else
-        begin        
+        begin
           Typing.typecheck_program file_parsed;
           if !type_only then ()
           else
             (); (* en attendant d'avoir fait la production de code*)
         end;
-    end
+    with
+      | Failure s -> signal_failure (Lexing.lexeme_start_p buf) s
+      | Parser.Error -> signal_failure (Lexing.lexeme_start_p buf) "Syntax error"
+      | exn -> signal_failure (Lexing.lexeme_start_p buf) (Printexc.to_string exn)
 
   with 
-    | Sys_error _ -> Printf.eprintf "%s : file not found" filename; exit 1
-    | Parser.Error -> failwith ""
-
-
+    | Sys_error _  -> Printf.eprintf "%s : file not found" filename; exit 1
 
 let main () = Arg.parse option_list main_exec ""
 
