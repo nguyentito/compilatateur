745e13b5c515e862b7b6269b07ae1565dc897fe7
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2012-11-28 20:23:20 +0100

    Retouche dans le main.

--- main.ml
+++ main.ml
@@ -1,20 +1,31 @@
 let type_only = ref false
 let parse_only = ref false
 
+let print_version () =
+  print_endline "MiniC : A compiler for a small subset of C\nVersion 0.42";
+  exit 0
+
+let usage_msg = "Usage : minic [options] file"
 let option_list = ["-parse-only", Arg.Set parse_only, 
                                   "Stops compiling after the parsing phase";
                    "-type-only",  Arg.Set type_only, 
                                   "Stops compiling after the typing phase";
+                   "--version",   Arg.Unit print_version,
+                                  "Display the program name and the version number"
                   ]
 
 
-(* Ne marche pas bien pour une bloc de code erroné sur plusieurs lignes *)
+(* The way this handles an error coming from an expression
+   spread on multiple lines is unsatisfying *)
 let signal_failure filename pos_start pos_end message =
+  let line_num = pos_start.Lexing.pos_lnum
+  and start_col = pos_start.Lexing.pos_cnum - pos_start.Lexing.pos_bol in
+  let end_col = if pos_end.Lexing.pos_lnum = line_num
+                then pos_end.Lexing.pos_cnum - pos_end.Lexing.pos_bol
+                else start_col
+  in
   Printf.eprintf "File \"%s\", line %d, characters %d-%d :\n%s\n"
-                 filename pos_start.Lexing.pos_lnum
-                 (pos_start.Lexing.pos_cnum - pos_start.Lexing.pos_bol)
-                 (pos_end.Lexing.pos_cnum - pos_end.Lexing.pos_bol)
-                 message;
+                 filename line_num start_col end_col message;
   exit 1
 
 let main_exec filename =
@@ -25,7 +36,6 @@ let main_exec filename =
                                           (Lexing.lexeme_start_p buf)
                                           (Lexing.lexeme_end_p buf)
                                           msg
-                                          
     in
     try
       let program_ast = Parser.parse_source_file Lexer.get_token buf in 
@@ -44,6 +54,9 @@ let main_exec filename =
   with 
     | Sys_error _  -> Printf.eprintf "%s : file not found" filename; exit 2
 
-let main () = Arg.parse option_list main_exec ""
+let main () =
+  if Array.length Sys.argv = 1
+  then Arg.usage option_list usage_msg
+  else Arg.parse option_list main_exec usage_msg
 
 let _ = main ()
