7ca07fa68af6d247c0706c3c59fb900f1ed73e8b
Nguyễn Lê Thành Dũng <nguyentito@gmail.com>
2014-04-16 14:04:42 +0200

    better testing script

--- scripts/test.hs
+++ scripts/test.hs
@@ -1,36 +1,84 @@
+-- Reused from the ML++ project
+
 import Control.Applicative
+import Control.Arrow
+import Control.Monad
+import Data.Char
 import Data.List
+import Data.Tuple
 import System.Directory
+import System.Environment
 import System.Exit
-import System.Process
+import System.IO
+import System.Process (system)
 import Text.Printf
 
-execTest cmd path = do
-  exitCode <- System.Process.system (printf "%s %s" cmd path)
-  putStrLn $ case exitCode of
-    ExitSuccess -> "Success"
-    ExitFailure 1 -> "Failure"
-    _ -> "Execution terminated abnormally"
-    
-runTests name dir testCmd = do
-  putStrLn name
-  fileList <- sort <$> getDirectoryContents ("tests/" ++ dir)
-  mapM_ runTest fileList
-  putStrLn ""
-    where runTest filename
-            | ".c" `isSuffixOf` filename = do
-                let path = printf "tests/%s/%s" dir filename
-                putStrLn path
-                -- todo : handle cases where the path is invalid
-                execTest testCmd path
-                putStrLn ""
-            | otherwise = pure ()
-  
+data TestCategory = Syntax
+                  | Typing
+                  deriving (Eq, Show)
+
+runTest cmd path = do
+  putStrLn $ "Testing " ++ path
+  exitCode <- system (printf "%s %s" cmd path)
+  case exitCode of
+    ExitSuccess   -> putStrLn "[[Success]]\n" >> return True
+    -- TODO: distinguish between rejected file and compilation error
+    ExitFailure _ -> putStrLn "[[Failure]]\n" >> return False
+
+runTests category isGood testCmd = do
+  putStrLn $ posneg ++ " tests for " ++ catName category ++ ":\n"
+  fileList <- sort . filter relevant <$> getDirectoryContents dir
+  let pathList = map (dir ++) fileList
+  results <- mapM (runTest (testCmd ++ flag)) pathList
+  let numSuccesses = length $ filter id results
+      numFailures  = length $ filter not results
+  putStrLn $ "Summary for " ++ dir ++ ":"
+  putStrLn $ show numSuccesses ++ " successes, " ++ show numFailures ++ " failures."
+  mapM_ (putStrLn . uncurry (++) . first oko) $ zip results fileList
+    where relevant = (".c" `isSuffixOf`)
+          dir = "tests/" ++ subdir category isGood
+          posneg = if isGood then "Positive" else "Negative"
+          oko b | b == isGood = "[OK] "
+                | otherwise   = "[KO] "
+          flag = case category of
+            Syntax -> " -parse-only" -- don't forget the space!
+            Typing -> " -type-only"
+
+catName = map toLower . show
+subdir category isGood =
+  catName category ++ if isGood then "/good/" else "/bad/"
+
+cleanDir category isGood = do
+  return ()
+  -- let dir = subdir category isGood
+  -- let exts Inference   = ["mle"]
+  --     exts Elaboration = ["mlr"]
+  --     exts Compilation = ["mle", "mlr", "ml", "cmo", "cmi"]
+  -- let f ext = system $ "rm " ++ dir ++ "*." ++ ext
+  -- mapM_ f $ exts category
+
+
+usageMsg = "Usage: runhaskell run-tests.hs (inference|elaboration) (good|bad) (clean)?"
+failWithUsage = do
+  hPutStrLn stderr usageMsg
+  exitWith $ ExitFailure 1
   
 main = do
-  runTests "Tests positifs de syntaxe"  "syntax/good" "./minic -parse-only";
-  runTests "Tests négatifs de syntaxe"  "syntax/bad"  "./minic -parse-only";
-  runTests "Tests positifs de typage"   "typing/good" "./minic -type-only";
-  runTests "Tests négatifs de typage"   "typing/bad"  "./minic -type-only";
-  runTests "Tests positifs d'exécution" "exec"        "./minic";
-  runTests "Tests négatifs d'exécution" "exec-fail"   "./minic"
+  args <- getArgs
+  when (not (length args `elem` [2,3])) failWithUsage
+  category <- case args !! 0 of
+    "syntax" -> pure Syntax
+    "typing" -> pure Typing
+    -- "codegen" -> pure Codegen
+    _ -> failWithUsage
+  isGood <- case args !! 1 of
+    "good" -> pure True
+    "bad"  -> pure False
+    _ -> failWithUsage
+  -- TODO: add option to run all tests / clean all directories
+  if (length args == 3)
+    then if args !! 2 == "clean"
+           then cleanDir category isGood
+           else failWithUsage
+                
+    else runTests category isGood "./minic"
