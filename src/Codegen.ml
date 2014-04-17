a1c96c0ba3d321b1344a68ea13306725e07dec1b
Nguyễn Lê Thành Dũng <nguyentito@gmail.com>
2014-04-17 15:15:44 +0200

    Better hello world

--- src/Codegen.ml
+++ src/Codegen.ml
@@ -6,8 +6,22 @@ open Mips
 (* Do not handle recursive calls to main!
    Anyway, they are forbidden by the C++ standard... *)
 
-let putchar =
-  label "putchar"
+(* No need for linking with outside: we can whoose any
+   naming convention whatsoever for labels!
+
+   global variables and functions get "global_" prefix
+   conditionals and loops get "local_" prefix
+*)
+
+(* global supply of fresh names starting with "local_" *)
+let gensym : unit -> string =
+  let r = ref 0 in
+  fun () -> let x = !r in r := x + 1;
+            "local_" ^ string_of_int x
+
+(* TODO: virer ce cérémonial *)
+let putchar : Mips.text =
+  label "global_putchar"
   ++ sub sp sp oi 8
   ++ sw ra areg (0, sp)
   ++ sw fp areg (4, sp)
@@ -17,40 +31,95 @@ let putchar =
   ++ li v0 11
   ++ syscall
 
+  ++ move v0 a0
+
   ++ add sp fp oi 4
   ++ lw ra areg (-4, fp)
   ++ lw fp areg (0, fp)
   ++ jr ra
 
-let compile_instr : instr -> Mips.text = fun i ->
-  match i with
-  | ExecExpr (Int, Apply (fn_id, [(Int, IntV x)])) ->
-    sub sp sp oi 8
-    ++ li a0 (Int32.to_int x) (* TODO: how to load big immediates? *)
-    ++ sw a0 areg (0, sp)
-    ++ jal fn_id
-    ++ add sp sp oi 8
-    
+let sbrk : Mips.text =
+  label "global_sbrk"
+  ++ nop (* TODO *)
+
+(* no stream fusion :-( *)
+let sequence : text list -> text
+  = fun xs -> List.fold_right (++) xs nop
+let seqmap : ('a -> text) -> 'a list -> text
+  = fun f xs -> sequence (List.map f xs)
+
+
+type stack_frame = string list (* loool *)
+
+let rec compile_expr : expr -> text = function
+  | (_, IntV x) -> li v0 (Int32.to_int x) (* TODO: how to load big immediates? *)
+  | (_, Apply (fn_id, args)) ->
+    seqmap eval_and_push args
+    ++ jal ("global_" ^ fn_id)
+    ++ add sp sp oi (4 * List.length args)
   | _ -> failwith "not supported yet"
 
-let compile_block : block -> Mips.text = fun b ->
-  let instrs = b.block_instrs in
-  List.fold_right (++) (List.map compile_instr instrs) nop
+and eval_and_push : expr -> text = function
+  | (Int, e) -> compile_expr (Int, e)
+                ++ sub sp sp oi 4
+                ++ sw v0 areg (0, sp)
+  | _ -> failwith "not supported yet"
+
+
+let compile_instr : instr -> text = fun i ->
+  match i with
+  | ExecExpr e -> compile_expr e
+  | _ -> failwith "not supported yet"
+
+let compile_block : block -> text = fun b ->
+  seqmap compile_instr b.block_instrs
+
+let compile_function : decl_fun -> text =
+  fun f -> label ("global_" ^ f.fun_name)
+           (* save $fp and $ra *)
+           ++ sub sp sp oi 8
+           ++ sw ra areg (0, sp)
+           ++ sw fp areg (4, sp)
+           ++ add fp sp oi 4
+
+           ++ compile_block f.fun_body
+
+           (* return in case the function doesn't call return;
+              (possible is the function returns void, or if it's main() *)
+           ++ add sp fp oi 4
+           ++ lw ra areg (-4, fp)
+           ++ lw fp areg (0, fp)
+           ++ jr ra
+
 
 let compile_program : Ast.Typed.program -> Mips.program
   = fun program ->
-    let funs = program.prog_funs in
-    let main_block = (List.find (fun x -> x.fun_name = "main") funs).fun_body in
-    
     { text =
+        (* entry point of the program
+           pass argc and argv to main() (whose label is global_main) *)
         label "main"
-        ++ compile_block main_block
-        ++ li v0 10
+        ++ sub sp sp oi 8
+        ++ sw a0 areg (4, sp)
+        ++ sw a1 areg (0, sp)
+        ++ jal "global_main"
+
+        (* exit with return value of main *)
+        ++ move a0 v0
+        ++ li v0 17 
         ++ syscall
 
-        ++ putchar;
+        (* all functions... *)
+        ++ seqmap compile_function program.prog_funs
 
-      data = nop
+        (* ++ compile_block main_block *)
+        (* ++ li v0 10 *)
+        (* ++ syscall *)
+
+        ++ putchar
+        ++ sbrk;
+      
+      data =
+        nop
     }
 
 
